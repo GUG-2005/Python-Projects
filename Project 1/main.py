0afd7da7e0873e82ce32d3135ac532deabdba2f3
Gugaan <111267744+GUG-2005@users.noreply.github.com>
2023-06-26 12:18:54 +0530

    Add files via upload

---
+++ Project 1/main.py
@@ -0,0 +1,12 @@
+#1. Create a greeting for your program.
+print("Seems like who's here to visit my First Program!!!")
+#2. Ask the user for the city that they grew up in.
+city = input("May I know where is your golden palace located?\n ")
+#3. Ask the user for the name of a pet.
+pet = input("Then tell me the name of your soul mate? \n")
+#4. Combine the name of their city and pet and show them their band name.
+band_name = str(city) +" "+ str(pet)
+#5. Make sure the input cursor shows on a new line:
+print("My suggestion for your Band Name would be " + str(band_name) + ", Sir!!")
+print("See You Soon...! Sir")
+
